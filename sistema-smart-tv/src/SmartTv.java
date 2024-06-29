5b75c9cc4e8a5d05bad1435baa72f4b5e4c08ea3
thiago <vitinhoeaice@gmail.com>
2024-06-29 20:35:22 -0300

    aula de metodos

---
+++ sistema-smart-tv/src/SmartTv.java
@@ -0,0 +1,35 @@
+public class SmartTv {
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 25;
+
+    public void mudarDeCanal(int novoCanal) {
+        canal = novoCanal;
+    }
+
+    public void avançarCanal() {
+        canal ++;
+    }
+
+    public void retrocederCanal() {
+        canal --;
+    }
+
+    public void aumentarVolume() {
+        volume ++;
+        System.out.println("Aumentando o volume para: " + volume);
+    }
+
+    public void diminuirVolume() {
+        volume --;
+        System.out.println("Diminuindo o volume para: " + volume);
+    }
+
+    public void ligar() {
+        ligada = true;
+    }
+
+    public void desligar() {
+        ligada = false;
+    }
+}
